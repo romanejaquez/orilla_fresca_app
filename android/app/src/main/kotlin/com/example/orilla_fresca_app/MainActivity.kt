a6f70417bb6cdeba07b6b89e6d87e028343ccce1
Roman Jaquez <romanjaquez@Romans-MacBook-Pro-3.local>
2021-02-07 19:18:22 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/orilla_fresca_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.orilla_fresca_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/helpers/iconhelper.dart
@@ -0,0 +1,10 @@
+class IconFontHelper {
+    static const String LOGO = 'a';
+    static const String MAIN_LOGO = 'b';
+    static const String MEATS = 'c';
+    static const String PASTRIES = 'd';
+    static const String SEEDS = 'e';
+    static const String SPICES = 'f';
+    static const String VEGS = 'g';
+    static const String FRUITS = 'h';
+}
